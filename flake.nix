0a52b3581b31b1ac71c7d4da1c6a05cb549320cf
MelkerI03 <Melker.Isaksson@outlook.com>
2026-03-09 10:53:46 +0100

    ✨ feat: add project compilation into executables

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A Nix-flake-based Python project";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
     { nixpkgs, ... }:
@@ -36,18 +36,66 @@
         ];
     in
     {
-
-      ######################################################################
-      # Package
-      ######################################################################
+      ###########
+      # Package #
+      ###########
 
       packages = forEachSupportedSystem (
         { pkgs }:
         let
           python = pkgs."python${concatMajorMinor pkgs version}";
+          py = python.withPackages (p: [
+            p.networkx
+            p.elasticsearch
+            p.python-dotenv
+            p.typer
+            p.pygraphviz
+          ]);
 
+          commonInputs = [
+            py
+            python.pkgs.nuitka
+            pkgs.graphviz
+          ];
+
+          makeNuitkaDerivation =
+            buildType:
+            pkgs.stdenv.mkDerivation {
+              pname = "es-map-${buildType}";
+              version = "0.1.0";
+              src = ./.;
+
+              nativeBuildInputs = [
+                py
+                pkgs.graphviz
+              ];
+
+              buildInputs = commonInputs;
+
+              buildPhase = ''
+                mkdir -p build
+
+                nuitka \
+                  --follow-imports \
+                  --show-progress \
+                  --output-dir=build \
+                  --output-filename=es-map \
+                  ${pkgs.lib.optionalString (buildType == "release") "--onefile --lto=yes"} \
+                  es_map/cli.py
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp build/es-map* $out/bin/es-map
+              '';
+
+              doCheck = false;
+            };
         in
         {
+          dev = makeNuitkaDerivation "dev";
+          release = makeNuitkaDerivation "release";
+
           default = python.pkgs.buildPythonApplication {
             pname = "es-map";
             version = "0.1.0";
@@ -61,24 +109,55 @@
               wheel
             ];
 
-            # Runtime dependencies go here
             propagatedBuildInputs = with python.pkgs; [
               networkx
               elasticsearch
               python-dotenv
               typer
-              matplotlib
               pygraphviz
             ];
 
-            doCheck = false; # As long as there arent any.
+            doCheck = false;
+          };
+
+          windows = pkgs.stdenv.mkDerivation {
+            pname = "es-map-win";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = [
+              py
+              pkgs.graphviz
+            ];
+
+            buildInputs = commonInputs;
+
+            buildPhase = ''
+              mkdir -p build
+
+              nuitka \
+                --follow-imports \
+                --show-progress \
+                --output-dir=build \
+                --output-filename=es-map.exe \
+                --onefile \
+                --lto=yes \
+                es_map/cli.py
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp build/es-map.exe $out/bin/es-map.exe
+            '';
+
+            doCheck = false;
           };
         }
       );
 
-      ######################################################################
-      # DevShell
-      ######################################################################
+      ############
+      # DevShell #
+      ############
 
       devShells = forEachSupportedSystem (
         { pkgs }:
@@ -95,7 +174,6 @@
               typer
               elasticsearch
               networkx
-              matplotlib
               pygraphviz
 
               pkgs.graphviz
