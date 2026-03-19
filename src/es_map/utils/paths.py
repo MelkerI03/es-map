46175f3f65ccd8d9cd565e9599ff0026f3e0f72a
MelkerI03 <Melker.Isaksson@outlook.com>
2026-03-19 09:54:18 +0100

    🧩 chore: remove unused definitions file

--- src/es_map/utils/paths.py
+++ src/es_map/utils/paths.py
@@ -2,18 +2,15 @@ from functools import lru_cache
 import sys
 from pathlib import Path
 
-from es_map.utils.definitions import ROOT_DIR
-
 
 @lru_cache(maxsize=1)  # Cache for 'single' lookup
 def get_root_path() -> Path:
     """
-    Get a path the project root
+    Get path to the project root
     """
     if hasattr(sys, "_MEIPASS"):
         path = Path(sys._MEIPASS)  # pyright: ignore[reportAttributeAccessIssue]
-        # path = ROOT_DIR
     else:
-        path = ROOT_DIR
+        path = Path(__file__).parents[1]  # ./..
 
     return path
